1af6b8e4061855a4233e33e96a6529d03618a2ea
y-tomita <y-tomita@MacBook-Pro.local>
2021-12-04 16:22:03 +0900

    初期設定デモ用

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,3 @@
 const { environment } = require('@rails/webpacker')
-const { VueLoaderPlugin } = require('vue-loader')
-const vue = require('./loaders/vue')
-
-environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
-environment.loaders.prepend('vue', vue)
-
-const cssLoader = environment.loaders.get('css')
-cssLoader.use=[{'loader':'vue-style-loader'}, {'loader': 'css-loader'}];
 
 module.exports = environment
